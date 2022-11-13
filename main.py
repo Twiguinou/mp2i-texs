1aee762c72ca9519072856a485d937aec9aa6681
Twiguinou <aksil.mammar@gmail.com>
2022-11-13 20:25:48 +0100

    zaefzefgezfef

--- main.py
+++ main.py
@@ -5,21 +5,18 @@ lbd2 = lbd ** 2
 mlbd_2 = -lbd * 2
 w_02 = 81649 ** 2
 
-def approx(t, xn, vn):
-    return xn + t * vn
+def calculate_next(t, in0, in1):
+    return (2 - 2*lbd*t)*in1 - (1 - 2*lbd*t + t**2*w_02)*in0
 
-def approx_derivative(t, xn, vn):
-    return vn + t * (-w_02 * xn + mlbd_2 * vn)
-
-def calc_values(period, time, x0, v0):
+def calc_values(period, time, i0, i1):
     n = int(time / period)
     indexes = [0] * n
+    indexes[1] = period
     values = [0] * n
-    values[0] = x0
-    prev_v = v0
-    for i in range(1, n):
-        values[i] = approx(period, values[i - 1], prev_v)
-        prev_v = approx_derivative(period, values[i - 1], prev_v)
+    values[0] = i0
+    values[1] = i1
+    for i in range(2, n):
+        values[i] = calculate_next(period, values[i - 2], values[i - 1])
         indexes[i] = i * period
     return indexes, values
 
@@ -29,11 +26,11 @@ from math import sqrt, exp
 import numpy as np
 def direct_method(t):
     vp = sqrt(lbd2 - w_02)
-    return 1 / (2 * L * vp) * (exp((-lbd + vp) * t) - exp((-lbd - vp) * t))
+    return -1 / (2 * L * vp) * (exp((-lbd + vp) * t) - exp((-lbd - vp) * t))
 
-T = 0.001
-#idxs, vls = calc_values(T, 0.0028, 0, -1 / L)
-#plt.plot(idxs, vls)
+T = 0.00000001
+idxs, vls = calc_values(T, 0.0028, 0, -T / L)
+plt.plot(idxs, vls)
 
 d = np.arange(0.0, 0.002, T)
 dv = np.vectorize(direct_method)(d)
